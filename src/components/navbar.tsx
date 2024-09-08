1d19e82f48369f0fa7a352e63f2ed1d9f404ce65
xLangley <alfr.galdames@duocuc.cl>
2024-09-08 01:56:30 -0300

    Changes

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react'
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import { signIn, useSession } from "next-auth/react";
+import { Separator } from "@/components/ui/separator"
+import { Session } from 'inspector/promises';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+
+function Navbar() {
+
+  const {data: Session} = useSession()
+  console.log("error")
+  console.log(Session)
+  
+  return (
+    <nav className='bg-slate-700 py-3 flex justify-between px-5 text-white items-center'>
+        <h1>
+            Señalando un futuro❤️
+        </h1>
+        <div>
+            <Button className='bg-white text-black px-6 py-2 rounded' variant="outline" onClick={() => signIn('google') }>Inicia sesion con Google</Button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
