36d89794d34fa09489e65d889311e2f461040060
xLangley <alfr.galdames@duocuc.cl>
2024-09-12 17:44:09 -0300

    Se crea login funcional con inicio de sesion y recuperacion de datos de usuario

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,38 +2,69 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Session } from 'inspector/promises';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import  Image  from "next/image";
 
 
 
 function Login() {
-  const { data: session, status } = useSession();
+  
+  const { data: session } = useSession();
 
-  const handleSignIn = () => {
-    signIn('google');
-  };
   return (
-    <>
-    <div className="flex items-center justify-center h-screen overflow-hidden">
+    <div className="flex items-center justify-center h-screen">
       <div className="grid grid-cols-9 grid-rows-7 gap-1">
         <div className="col-span-3 row-span-7 col-start-2">
-          Aca va el logo o la foto nose 
+          <Image
+            className="rounded-full"
+            src="https://i.gifer.com/origin/3f/3f902c487567cbb8201d619bbc6b5af2.gif"
+            alt=""
+            width={400}
+            height={400}
+          />
         </div>
-
-        <div className="col-span-3 row-span-7 col-start-6">
-          <Button className='bg-white text-black px-1 py-2 rounded flex items-center space-x-2' variant="outline" onClick={handleSignIn}>
-            <Avatar className='w-8 h-8'>
-              <AvatarImage src="https://pngimg.com/uploads/google/google_PNG19635.png" />
-            </Avatar>
-            <span className='px-2'>Inicia sesión con Google</span>
-          </Button>
-        </div>
+        <>
+          {session?.user ? (
+            <>
+              <div className="col-span-3 row-span-7 col-start-6 row-start-4 flex justify-center">
+                <Button className="bg-white text-black rounded-full items-center" variant="outline" onClick={() => signOut()}>
+                  <Avatar className="w-8 h-8">
+                    <AvatarImage src="https://static.vecteezy.com/system/resources/previews/000/574/782/original/vector-logout-sign-icon.jpg" />
+                  </Avatar>
+                  <span className="px-1">Cerrar sesión</span>
+                </Button>
+              </div>
+              <div className="col-span-3 row-span-7 col-start-6 row-start-5 flex justify-center">
+                <p>{session.user.name} </p>
+                {/* <Avatar className="w-8 h-8 rounded-full">
+                  <AvatarImage src={session.user.image}/>
+                </Avatar> */}
+              </div>
+              <div className=" text-red-600 col-span-3 row-span-7 col-start-6 row-start-6 flex justify-center">
+                <p>{session.user.email}</p>
+              </div>
+              <div className=" text-red-600 col-span-3 row-span-7 col-start-6 row-start-7 flex justify-center">
+                <Avatar className="w-10 h-10">
+                  <AvatarImage src= {session.user.image}/> {/* Funciona igual dejenlo */}
+                </Avatar>
+              </div>
+            </>
+          ) : (
+            <div className="col-span-3 row-span-7 col-start-6 row-start-4 flex justify-center">
+              <Button className="bg-white text-black rounded-full items-center" variant="outline" onClick={() => signIn('google')}>
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src="https://pngimg.com/uploads/google/google_PNG19635.png" />
+                </Avatar>
+                <span className="px-1">Inicia sesión con Google</span>
+              </Button>
+            </div>
+          )}
+        </>
       </div>
-    </div>    
-    </>
-  )
+    </div>
+  );  
 }
 
 export default Login
