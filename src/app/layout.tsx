416138fcb2929f12ba8676371c8594dfddffb6bb
Fernando Muñoz <fernandofailed@gmail.com>
2024-09-13 21:12:07 -0300

    Se hacen cambios en el contenedor principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "aos/dist/aos.css"; // Importa los estilos de AOS
 import { metadata } from './metadata';
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
 import { Providers } from "./providers";
 
@@ -34,12 +34,14 @@ export default function RootLayout({
   }, []);
 
   return (
-    <html lang="en">
+    <html lang="es">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Providers>
             <Navbar />
-            {children}
+            <div className="min-h-screen min-w-screen pt-4 pb-4 md:pr-16 md:pl-16 lg:pr-56 lg:pl-56">
+              {children}
+            </div>
             <Footer />
           </Providers>
       </body>
