a8522169735f89ed654dc44c27aff7d5d9487238
Fernando Muñoz <fernandofailed@gmail.com>
2024-09-14 04:04:09 -0300

    Se hace el login responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,57 +14,34 @@ function Login() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="grid grid-cols-9 grid-rows-7 gap-1">
-        <div className="col-span-3 row-span-7 col-start-2">
-          <Image
-            className="rounded-full"
-            src="https://i.gifer.com/origin/3f/3f902c487567cbb8201d619bbc6b5af2.gif"
-            alt=""
-            width={400}
-            height={400}
-          />
-        </div>
+    <div className='flex flex-col lg:flex-row items-center justify-center gap-20 md:justify-evenly lg:flex-wrap h-[75vh]'>
+      <section className=''>
+        <Image
+          className="rounded-full w-[30vh] md:w-[35vh] aspect-square"
+          src="https://i.gifer.com/origin/3f/3f902c487567cbb8201d619bbc6b5af2.gif"
+          alt=""
+          width={0}
+          height={0}
+        />
+      </section>
+      {session?.user ? (
         <>
-          {session?.user ? (
-            <>
-              <div className="col-span-3 row-span-7 col-start-6 row-start-4 flex justify-center">
-                <Button className="bg-white text-black rounded-full items-center" variant="outline" onClick={() => signOut()}>
-                  <Avatar className="w-8 h-8">
-                    <AvatarImage src="https://static.vecteezy.com/system/resources/previews/000/574/782/original/vector-logout-sign-icon.jpg" />
-                  </Avatar>
-                  <span className="px-1">Cerrar sesión</span>
-                </Button>
-              </div>
-              <div className="col-span-3 row-span-7 col-start-6 row-start-5 flex justify-center">
-                <p>{session.user.name} </p>
-                {/* <Avatar className="w-8 h-8 rounded-full">
-                  <AvatarImage src={session.user.image}/>
-                </Avatar> */}
-              </div>
-              <div className=" text-red-600 col-span-3 row-span-7 col-start-6 row-start-6 flex justify-center">
-                <p>{session.user.email}</p>
-              </div>
-              <div className=" text-red-600 col-span-3 row-span-7 col-start-6 row-start-7 flex justify-center">
-                <Avatar className="w-10 h-10">
-                  <AvatarImage src={session.user.image!}/> {/* Funciona igual dejenlo */}
-                </Avatar>
-              </div>
-            </>
-          ) : (
-            <div className="col-span-3 row-span-7 col-start-6 row-start-4 flex justify-center">
-              <Button className="bg-white text-black rounded-full items-center" variant="outline" onClick={() => signIn('google')}>
-                <Avatar className="w-8 h-8">
-                  <AvatarImage src="https://pngimg.com/uploads/google/google_PNG19635.png" />
-                </Avatar>
-                <span className="px-1">Inicia sesión con Google</span>
-              </Button>
-            </div>
-          )}
+          <section className=''> 
+          </section>
         </>
-      </div>
+        ) : (
+          <div className="">
+            <Button className="rounded-full" variant="outline" onClick={() => signIn('google')}>
+              <Avatar className="h-8 w-8">
+                <AvatarImage src="https://pngimg.com/uploads/google/google_PNG19635.png" />
+              </Avatar>
+              <span className="px-1">Inicia sesión con Google</span>
+            </Button>
+          </div>
+        )
+      }
     </div>
-  );  
+  );
 }
 
 export default Login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 
 function HomePage() {
   return (
+    <>
     <div>HomePage</div>
+    </>
   )
 }
 
