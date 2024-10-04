05f6dfdaadfef83781349467c865e5193b10f809
Fernando Muñoz <fernandofailed@gmail.com>
2024-10-04 20:44:03 -0300

    Se arreglan colores de estilos del 'button'

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-primary-500",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive-600",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary-500 hover:text-white",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         fullGhost: "",
         link: "text-primary underline-offset-4 hover:underline",
