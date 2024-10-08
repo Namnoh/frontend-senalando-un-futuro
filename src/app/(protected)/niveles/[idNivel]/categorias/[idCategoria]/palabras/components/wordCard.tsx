c66ea953163b091818f4b7043f5250af2d4ac217
Fernando Muñoz <fernandofailed@gmail.com>
2024-10-07 23:00:26 -0300

    Se deja el background de palabras como sale en mockup

--- src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/components/wordCard.tsx
+++ src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/components/wordCard.tsx
@@ -11,8 +11,8 @@ export const WordsCards = ({idNivel, idCategoria, word}: {idNivel:number, idCate
             href={`/niveles/${idNivel}/categorias/${idCategoria}/palabras/${w.id}`}
             className='group'
         >
-            <div className='flex items-center justify-center border-2 border-accent-100 rounded-2xl p-5 relative shadow-md'>
-                {IconToRender && <IconToRender className="h-16 w-16 lg:h-20 lg:w-20 text-white" />}
+            <div className='flex items-center justify-center border-2 border-accent-100 rounded-2xl p-5 relative shadow-md bg-white'>
+                {IconToRender && <IconToRender className="h-16 w-16 lg:h-20 lg:w-20 text-black" />}
                 <ProgressBadge status={w.status}/>
                 <div className="absolute inset-0 bg-accent-600 opacity-0 group-hover:opacity-70 transition-opacity duration-300 ease-in-out rounded-2xl"/>
                 <div className='absolute h-max opacity-0 group-hover:opacity-100 text-white font-medium transition-opacity duration-300 ease-in-out lg:text-xl'>
