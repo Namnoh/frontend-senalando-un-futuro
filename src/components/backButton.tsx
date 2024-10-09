198f6ea4adc8be5bc6967a1c966523b676e8840a
Fernando Muñoz <fernandofailed@gmail.com>
2024-10-08 22:36:56 -0300

    Se corrige z-index del botón de back

--- src/components/backButton.tsx
+++ src/components/backButton.tsx
@@ -11,7 +11,7 @@ export default function BackButton({actualRoute, isMobile}: {actualRoute:string,
         <Button
             variant={`${isWordsSection ? 'fullGhost' : 'default'}`}
             onClick={() => router.back()}
-            className={`fixed z-10 ${isMobile ? 'top-5 right-5' : 'top-7 right-7'} ${isWordsSection ? 'text-white' : ''}`}
+            className={`fixed z-50 ${isMobile ? 'top-5 right-5' : 'top-7 right-7'} ${isWordsSection ? 'text-white' : ''}`}
         >
             <ArrowLeft className="h-5 w-5" />
         </Button>
