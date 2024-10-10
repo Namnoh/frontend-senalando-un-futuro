698b090075185d97ec3c843ae88a09119d456870
Fernando Muñoz <fernandofailed@gmail.com>
2024-10-10 09:46:45 -0300

    Se crea servicio provisional para usuario

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -7,7 +7,7 @@ const users:Usuario[] = [
     {idUsuario: 3, nombreUsuario: 'Jean', apellidoUsuario: 'Venegas', correoUsuario:'je.venegasa@duocuc.cl', idRol:0}
 ]
 
-export async function getCategory(idUsuario:number) {
+export async function getUsers(idUsuario:number) {
     // TODO: Revisar qué pasa cuando la petición se demora
     // await new Promise((resolve) => setTimeout(resolve, 3000))
     const user = users.find(user => user.idUsuario === Number(idUsuario));
@@ -17,6 +17,6 @@ export async function getCategory(idUsuario:number) {
 // TODO: validar que el usuario tiene acceso a ese nivel de donde pide la categoría
 export async function getAllUsers():Promise<Usuario[]> {
     // await new Promise((resolve) => setTimeout(resolve, 3000))
-    const usersList = users;
-    return usersList;
+    // const usersList = users;
+    return users;
 };
