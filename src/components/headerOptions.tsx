9ee0951634f3795751e8e99c4b5c52e1b1369cd1
Fernando Muñoz <fernandofailed@gmail.com>
2024-10-11 20:42:21 -0300

    Se crea header

---
+++ src/components/headerOptions.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+import { AllSidebars } from "@/components/navbar/";
+// import { showSidebar } from '@/services/sidebar.service';
+import { useMediaQuery } from '@/hooks/uiHooks';
+import BackButton from '@/components/backButton';
+
+export default function HeaderOptions() {
+const actualRoute = usePathname();
+// const showSide = showSidebar({pathName: actualRoute});
+const isMobile = useMediaQuery('(max-width: 768px)');
+
+    return (
+        <>
+            {/* Sidebars */}
+            { isMobile != undefined && (
+                    <AllSidebars actualRoute={actualRoute} isMobile={isMobile}/>
+                )}
+                {/* Votón para volver */}
+                <BackButton actualRoute={actualRoute} isMobile={isMobile}/>
+                {/* Contenido Principal */}
+        </>
+    )
+}
