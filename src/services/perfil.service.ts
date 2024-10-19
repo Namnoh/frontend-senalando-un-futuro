11e3f3dd8b0e672b1118a66d0a114629fd82337a
je4n83 <jean.vtedy@gmail.com>
2024-10-19 00:00:51 -0300

    se agregan apis

--- src/services/perfil.service.ts
+++ src/services/perfil.service.ts
@@ -1,27 +1,18 @@
 import { perfil } from '@/interfaces/perfilinterface'
+import { Usuario } from '@/interfaces/usuarioInterface'
 
 // esta vaina representa la API
-    let perfilMock: perfil = {
-        idPerfil: 1, correoUsuario: 'usuario@ejemplo.com',contrasena: 'contraseña_encriptada', nombrePerfil: 'Miau Venegas'}
-    
-    export const perfilService = {
-        obtenerPerfil: async (): Promise<perfil> => {
-        // Esto simula la llama pa la API 
-        await new Promise(resolve => setTimeout(resolve, 500))
-        return { ...perfilMock, contrasena: '' } //En el vidio vi que esta vaina hace que no se devuelva la contraseña
-        },
-    
-        actualizarPerfil: async (perfil: Partial<perfil>): Promise<perfil> => {
-        // tambien simula la llama pa la api pero pal perfil chavalin
-        await new Promise(resolve => setTimeout(resolve, 500))
-        perfilMock = { ...perfilMock, ...perfil }
-        return { ...perfilMock, contrasena: '' } 
-        },
-    
-        cambiarContraseña: async (nuevaContraseña: string): Promise<void> => {
-        // aqui se supone que se cambia la contraseña
-        await new Promise(resolve => setTimeout(resolve, 500))
-        perfilMock.contrasena = nuevaContraseña //creo que si la conectamos aqui funcionaria de pana
-        console.log('Contraseña cambiada') //y esto por que soi choro pa que muestre si se cambio
+    export async function fetchUser(idUsuario: number): Promise<Usuario> {
+        try {
+            const response = await fetch(`${process.env.API_URL}/users/${idUsuario}`);
+            if (!response.ok) {
+                throw new Error('Error al obtener el progreso del usuario');
+            }
+            const data: Usuario = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error en fetchUserProgress:', error);
+            throw error; // Lanza el error para que el componente pueda manejarlo
         }
     }
+    
