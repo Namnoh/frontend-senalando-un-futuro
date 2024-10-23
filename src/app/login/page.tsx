d9586fc05cee1a1afb74b1d97bf03e17304c1dac
xLangley <alfr.galdames@duocuc.cl>
2024-10-23 00:58:02 -0300

    se corrigen cambios

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ import { signIn } from "next-auth/react"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import styles from "@/app/styles/auth.module.scss";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -23,9 +24,9 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card"
 
 const formSchema = z.object({
@@ -35,7 +36,7 @@ const formSchema = z.object({
   contrasenaUsuario: z.string().min(8, {
     message: "La contraseña debe tener al menos 8 caracteres.",
   }),
-});
+})
 
 export default function LoginPage() {
   const router = useRouter()
@@ -68,95 +69,97 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex flex-col lg:flex-row-reverse items-center justify-center gap-1 sm:gap-6 md:justify-evenly lg:flex-wrap min-h-screen">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-center">Iniciar sesión</CardTitle>
-          <CardDescription className="text-center">
-            Ingresa tus credenciales para acceder
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="correoUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Correo electrónico</FormLabel>
-                    <FormControl>
-                      <Input type="email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+    <div className={styles.backgroundImageLogin}>
+      <div className="flex flex-col lg:flex-row-reverse items-center justify-center gap-1 sm:gap-6 md:justify-evenly lg:flex-wrap min-h-screen">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle className="text-center">Iniciar sesión</CardTitle>
+            <CardDescription className="text-center">
+              Ingresa tus credenciales para acceder
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="correoUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Correo electrónico</FormLabel>
+                      <FormControl>
+                        <Input type="email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="contrasenaUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contraseña</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {loginError && (
+                  <div className="text-red-500 text-sm">{loginError}</div>
                 )}
-              />
-              <FormField
-                control={form.control}
-                name="contrasenaUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Contraseña</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {loginError && (
-                <div className="text-red-500 text-sm">{loginError}</div>
-              )}
-              <Button type="submit" className="w-full">
-                Iniciar sesión
-              </Button>
-            </form>
-          </Form>
-          <div className="relative my-4">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
+                <Button type="submit" className="w-full">
+                  Iniciar sesión
+                </Button>
+              </form>
+            </Form>
+            <div className="relative my-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  O continúa con
+                </span>
+              </div>
             </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                O continúa con
-              </span>
-            </div>
-          </div>
-          <Button variant="outline" className="w-full" onClick={handleGoogleSignIn}>
-            <svg
-              className="mr-2 h-4 w-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-            Iniciar sesión con Google
-          </Button>
-        </CardContent>
-        <CardFooter className="flex flex-col justify-center gap-2">
-          <p>
-            <Link href="/recover" className="text-primary hover:underline">
-              Recuperar contraseña
-            </Link>
-          </p>
-          <p className="text-sm text-muted-foreground">
-            ¿No tienes una cuenta?{" "}
-            <Link href="/register" className="text-primary hover:underline">
-              Regístrate
-            </Link>
-          </p>
-        </CardFooter>
-      </Card>
-      <div className="hidden md:block bg-white outline w-[500px] h-[500px] rounded-full"></div>
+            <Button variant="outline" className="w-full" onClick={handleGoogleSignIn}>
+              <svg
+                className="mr-2 h-4 w-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                ></path>
+              </svg>
+              Iniciar sesión con Google
+            </Button>
+          </CardContent>
+          <CardFooter className="flex flex-col justify-center gap-2">
+            <p>
+              <Link href="/recover" className="text-primary hover:underline">
+                Recuperar contraseña
+              </Link>
+            </p>
+            <p className="text-sm text-muted-foreground">
+              ¿No tienes una cuenta?{" "}
+              <Link href="/register" className="text-primary hover:underline">
+                Regístrate
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+        <div className="hidden md:block bg-white outline w-[500px] h-[500px] rounded-full"></div>
+      </div>
     </div>
   )
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,8 @@ import { useRouter } from "next/navigation"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
+import styles from "@/app/styles/auth.module.scss";
+import { signIn } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -17,14 +18,14 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 const formSchema = z.object({
-  nombreUsuario: z.string().min(3, {
-    message: "El nombre debe tener al menos 3 caracteres.",
+  nombreUsuario: z.string().min(2, {
+    message: "El nombre debe tener al menos 2 caracteres.",
   }),
-  apellidoUsuario: z.string().min(3, {
-    message: "El apellido debe tener al menos 3 caracteres.",
+  apellidoUsuario: z.string().min(2, {
+    message: "El apellido debe tener al menos 2 caracteres.",
   }),
   correoUsuario: z.string().email({
     message: "Debe ser un correo electrónico válido.",
@@ -79,125 +80,131 @@ export default function RegisterPage() {
     router.push('/login')
   }
 
+  const handleGoogleSignIn = () => {
+    signIn("google", { callbackUrl: "/niveles" })
+  }
+
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center gap-5 sm:gap-6 md:justify-evenly lg:flex-wrap min-h-screen p-4">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-center">Registrarse</CardTitle>
-          <CardDescription className="text-center">
-            Crea una nueva cuenta
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="nombreUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nombre</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="apellidoUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Apellido</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="correoUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Correo electrónico</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="contrasenaUsuario"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Contraseña</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Confirmar contraseña</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" className="w-full">Registrarse</Button>
-            </form>
-          </Form>
-          <div className="relative my-4">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
+    <div className={styles.backgroundImageRegister}>
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-5 sm:gap-6 md:justify-evenly lg:flex-wrap min-h-screen p-4">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle className="text-center">Registrarse</CardTitle>
+            <CardDescription className="text-center">
+              Crea una nueva cuenta
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="nombreUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nombre</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="apellidoUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Apellido</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="correoUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Correo electrónico</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="contrasenaUsuario"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contraseña</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirmar contraseña</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="w-full">Registrarse</Button>
+              </form>
+            </Form>
+            <div className="relative my-4">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  O continúa con
+                </span>
+              </div>
             </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                O continúa con
-              </span>
-            </div>
-          </div>
-          <Button variant="outline" className="w-full">
-            <svg
-              className="mr-2 h-4 w-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-            Registrarse con Google
-          </Button>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <p className="text-sm text-muted-foreground">
-            ¿Ya tienes una cuenta?{" "}
-            <Link href="/login" className="text-primary hover:underline">
-              Inicia sesión
-            </Link>
-          </p>
-        </CardFooter>
-      </Card>
-      <div className="hidden md:block bg-white outline w-[500px] h-[500px] rounded-full"></div>
+            <Button variant="outline" className="w-full" onClick={handleGoogleSignIn}>
+              <svg
+                className="mr-2 h-4 w-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                ></path>
+              </svg>
+              Registrarse con Google
+            </Button>
+          </CardContent>
+          {/* <CardFooter className="flex justify-center">
+            <p className="text-sm text-muted-foreground">
+              ¿Ya tienes una cuenta?{" "}
+              <Link href="/login" className="text-primary hover:underline">
+                Inicia sesión
+              </Link>
+            </p>
+          </CardFooter> */}
+        </Card>
+        <div className="hidden md:block bg-white outline w-[500px] h-[500px] rounded-full"></div>
+      </div>
     </div>
   )
 }
