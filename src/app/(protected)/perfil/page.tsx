7292965601b08e426a00dee9c9130c48efaa10d2
je4n83 <jean.vtedy@gmail.com>
2024-10-29 20:27:11 -0300

    Se agrega backend y mejoras a la page de perfil

--- src/app/(protected)/perfil/page.tsx
+++ src/app/(protected)/perfil/page.tsx
@@ -1,56 +1,50 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { perfilService } from '@/services/perfil.service'
 import { getLevel } from '@/services/common.service'
-import { perfil } from '@/interfaces/perfilinterface'
 import { Nivel, UserProgress } from '@/interfaces/levelinterface'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { DynamicIcon } from "@/components/customUI/dynamicLucideIcon"
 import styles from '@/app/styles/home.module.scss'
 import SimpleLoading from '@/components/customUI/simpleLoading'
 import CambiarContrasena from './components/cambiarContrasena'
-import Email from './components/email'
 import PalabraFavorita from './components/palabraFavorita'
 import Progreso from './components/progreso'
 import { useProgressContext } from '@/contexts/userProgressContext'
+import { useSession } from 'next-auth/react'
 
 
 export default function PerfilPage() {
-    const [perfil, setPerfil] = useState<perfil | null>(null)
     const [userProgress, setUserProgress] = useState<UserProgress | null>(null)
     const [niveles, setNiveles] = useState<Nivel[]>([])
     const [isLoading, setIsLoading] = useState(true)
     const { progress } = useProgressContext();
+    const { data: session } = useSession();
 
     useEffect(() => {
         const cargarDatos = async () => {
-            setIsLoading(true)
+            setIsLoading(true);
             try {
-                if (progress === null) {
-                    throw new Error('El progreso del usuario no está disponible');
+                if (!session || !session.user || !progress) {
+                    throw new Error('Los datos del usuario o progreso no están disponibles');
                 }
-                const perfilData = await perfilService.obtenerPerfil()
-                setPerfil(perfilData)
                 const nivelesData = await Promise.all([
-                    getLevel(1,progress),
-                    getLevel(2,progress),
-                    getLevel(3,progress)
-                ])
-                setNiveles(nivelesData.filter((nivel): nivel is Nivel => nivel !== null))
-                
-                setUserProgress(progress)
+                    getLevel(1, progress),
+                    getLevel(2, progress),
+                    getLevel(3, progress),
+                ]);
+                setNiveles(nivelesData.filter((nivel): nivel is Nivel => nivel !== null));
+                setUserProgress(progress);
             } catch (error) {
-                console.error("Error al cargar los datos:", error)
+                console.error("Error al cargar los datos:", error);
             } finally {
-                setIsLoading(false)
+                setIsLoading(false);
             }
-        }
-        cargarDatos()
-    }, [])
-
+        };
+        cargarDatos();
+    }, [session, progress]);
+    
     if (isLoading) return <SimpleLoading />
-
     return (
         <div className={styles.backgroundPerfil}>
             <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
@@ -60,11 +54,10 @@ export default function PerfilPage() {
                             <div className="w-24 h-24 bg-white rounded-full flex items-center justify-center shadow-md">
                                 <DynamicIcon name="User" classes="h-12 w-12 text-gray-400" />
                             </div>
-                            <h2 className="mt-4 text-2xl lg:text-4xl font-semibold text-primary-400">{perfil?.nombrePerfil || 'Usuario'}</h2>
-                            <p className="text-xl font-semibold lg:text-3xl text-secondary-300">ID: {perfil?.idPerfil}</p>
+                            <h2 className="mt-4 text-2xl lg:text-4xl font-semibold text-primary-400">{session?.user?.name} {session?.user?.lastname}</h2>
                         </div>
                     </div>
-                    <div className="px-6 -mt-8 mb-4 p-8 relative">
+                    <div className="px-6 -mt-8 mb-4 p-8 relative text-black">
                         <Tabs defaultValue="profile" className="w-full relative">
                             <TabsList className="grid grid-cols-2 rounded-xl bg-white shadow-md h-16 -top-10 w-40 sm:h-20 sm:w-60 lg:w-96 left-1/2 transform -translate-x-1/2 absolute z-10">
                                 <TabsTrigger value="profile" className="rounded-l-xl data-[state=active]:bg-secondary-400 data-[state=active]:text-white h-full flex items-center justify-center">
@@ -77,7 +70,8 @@ export default function PerfilPage() {
                                 </TabsTrigger>
                             </TabsList>
                             <TabsContent value="profile" className="space-y-6 p-6 bg-secondary-50 lg:p-20 rounded-3xl shadow-2xl shadow-primary-500">
-                                {perfil && <Email email={perfil.correoUsuario} />}
+                                <h3 className="pt-10 sm:pt-6 lg:pt-6 sm:text-xl lg:text-2xl text-lg font-semibold text-black ">Correo:</h3>
+                                <p className="pb-5 sm:text-xl lg:text-2xl lg:ml-10 text-black">{session?.user.email}</p>
                                 <CambiarContrasena />
                                 {userProgress && niveles.length > 0 && (
                                     <Progreso niveles={niveles} userProgress={userProgress} />
