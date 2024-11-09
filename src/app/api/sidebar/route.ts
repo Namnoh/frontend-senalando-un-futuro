eb1ba6734df11d14896ebb311542d227c7dcc87f
Fernando Muñoz <fernandofailed@gmail.com>
2024-11-08 23:52:44 -0300

    Se solucionan rutas para deploy

--- src/app/api/sidebar/route.ts
+++ src/app/api/sidebar/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { getSidebarLinks } from "@/services/sidebar.service";
 import { Vista } from '@/interfaces/vistaInterface';
 
