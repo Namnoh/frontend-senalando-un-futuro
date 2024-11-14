9ed686a315eea137abe3125fe37d634a7a29fdde
xLangley <alfr.galdames@duocuc.cl>
2024-11-14 01:58:52 -0300

    Icon in navbar

--- src/components/navbar/profileCard.tsx
+++ src/components/navbar/profileCard.tsx
@@ -3,17 +3,26 @@ import Link from "next/link";
 import { Card, CardTitle } from "../ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { useSession } from "next-auth/react";
+import Image from 'next/image';
+import { DynamicIcon } from "../customUI/dynamicLucideIcon";
 
 export default function ProfileCard() {
     const { data: session } = useSession();
     return (
         <Link href={'/perfil'}>
             <Card>
-                <div className="flex items-center p-3 md:p-4">
-                    <Avatar className="w-14 h-14"> 
-                        <AvatarImage className="w-full h-full object-cover rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s" alt="Logo" />
-                        <AvatarFallback>AI</AvatarFallback>
-                    </Avatar>
+                <div className="flex items-center p-3 gap-3 md:p-4">
+                    {session?.user?.image ? (
+                        <Image
+                        src={session.user.image} 
+                        alt="Perfil del usuario"
+                        width={50} 
+                        height={50} 
+                        className="first-letter:object-cover rounded-full"
+                        />
+                        ) : (
+                        <DynamicIcon name="User" classes="h-12 w-12 text-gray-400" />
+                    )}
                     <div className="flex flex-grow flex-col items-center justify-center">
                         <CardTitle>{session?.user.name} {session?.user.lastname}</CardTitle>
                         <span className="text-sm md:text-base">{session?.user.email}</span>
