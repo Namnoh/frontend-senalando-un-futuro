a548e62b16fb2eaf63971ccf3d729fbcdd5d1f29
je4n83 <jean.vtedy@gmail.com>
2024-12-04 03:07:27 -0300

    Refactor : Se agrega mejoras al desktop-video.tsx

--- src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/[idPalabras]/components/vide/desktop-video.tsx
+++ src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/[idPalabras]/components/vide/desktop-video.tsx
@@ -8,6 +8,7 @@ import { Slider } from "@/components/ui/slider"
 import { Input } from "@/components/ui/input"
 import { Repeat } from "lucide-react"
 import { Palabra } from '@/interfaces/palabraInterface'
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 export default function DesktopVideo({word}: {word:Palabra}) {
   const playerRef = useRef<ReactPlayer>(null)
@@ -31,46 +32,56 @@ export default function DesktopVideo({word}: {word:Palabra}) {
     }
   }, [])
 
-  useEffect(() => {
-    handleVideoURL(word.videoPalabra)
-  }, [])
+  //useEffect(() => {
+  //  handleVideoURL(word.videoPalabra)
+  //}, [word.videoPalabra, handleVideoURL])
+
+  const videoAvailable = typeof word.videoPalabra === 'string' && word.videoPalabra.toLowerCase().startsWith('http')
 
   return (
     <div className="h-full">
       <Card className="h-full w-full max-w-3xl mx-auto">
         <CardContent className='h-full flex flex-col items-center p-5'>
-          <div className="flex-grow mb-4">
-            <ReactPlayer
-              ref={playerRef}
-              url={videoUrl}
-              width="100%"
-              height="100%"
-              playbackRate={playbackRate}
-              playing={isPlaying}
-              onReady={() => console.log('Video ready')}
-              onError={(e) => console.error('Error loading video:', e)}
-              onEnded={() => setIsPlaying(false)}
-            />
-          </div>
-          <div className='flex justify-between w-full'>
-            <div className="mb-4">
-              <label htmlFor="playback-rate" className="block text-sm font-medium mb-2">
-                Velocidad: {playbackRate.toFixed(2)}x
-              </label>
-              <Slider
-                id="playback-rate"
-                min={0.25}
-                max={2}
-                step={0.25}
-                value={[playbackRate]}
-                onValueChange={handlePlaybackRateChange}
-              />
-            </div>
-            <Button onClick={replayVideo} className="flex items-center gap-2">
-              <Repeat className="w-4 h-4" />
-              Repetir
-            </Button>
-          </div>
+        {videoAvailable ? (
+            <>
+              <div className="flex-grow mb-4 w-full">
+                <ReactPlayer
+                  ref={playerRef}
+                  url={word.videoPalabra}
+                  width="100%"
+                  height="100%"
+                  playbackRate={playbackRate}
+                  playing={isPlaying}
+                  onReady={() => console.log('Video listo')}
+                  onError={(e) => console.error('Error al cargar el video:', e)}
+                  onEnded={() => setIsPlaying(false)}
+                />
+              </div>
+              <div className='flex justify-between w-full'>
+                <div className="mb-4 flex-grow mr-4">
+                  <label htmlFor="playback-rate" className="block text-sm font-medium mb-2">
+                    Velocidad: {playbackRate.toFixed(2)}x
+                  </label>
+                  <Slider
+                    id="playback-rate"
+                    min={0.25}
+                    max={2}
+                    step={0.25}
+                    value={[playbackRate]}
+                    onValueChange={handlePlaybackRateChange}
+                  />
+                </div>
+                <Button onClick={replayVideo} className="flex items-center gap-2">
+                  <Repeat className="w-4 h-4" />
+                  Repetir
+                </Button>
+              </div>
+            </>
+          ):(
+            <div className="flex items-center justify-center h-full">
+              <p className="text-lg font-medium text-primary-500">Video de referencia no disponible</p>
+            </div> 
+          )}
         </CardContent>
       </Card>
     </div>
--- src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/[idPalabras]/components/vide/mobile-video.tsx
+++ src/app/(protected)/niveles/[idNivel]/categorias/[idCategoria]/palabras/[idPalabras]/components/vide/mobile-video.tsx
@@ -8,6 +8,7 @@ import { Slider } from "@/components/ui/slider"
 import { Input } from "@/components/ui/input"
 import { Repeat } from "lucide-react"
 import { Palabra } from '@/interfaces/palabraInterface'
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 export default function MobileVideo({word}:{word:Palabra}) {
   const playerRef = useRef<ReactPlayer>(null)
@@ -31,43 +32,55 @@ export default function MobileVideo({word}:{word:Palabra}) {
     }
   }, [])
 
-  useEffect(() => {
-    handleVideoURL(word.videoPalabra)
-  }, [])
+  //useEffect(() => {
+  //  handleVideoURL(word.videoPalabra)
+  //}, [])
+
+  const videoAvailable = typeof word.videoPalabra === 'string' && word.videoPalabra.toLowerCase().startsWith('http')
   return (
     <div className="my-4">
       <Card>
         <CardContent className='p-3'>
-          <div className="aspect-square mb-2">
-            <ReactPlayer
-              ref={playerRef}
-              url={videoUrl}
-              width="100%"
-              height="100%"
-              playbackRate={playbackRate}
-              playing={isPlaying}
-              onReady={() => console.log('Video ready')}
-              onError={(e) => console.error('Error loading video:', e)}
-              onEnded={() => setIsPlaying(false)}
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="playback-rate" className="block text-sm font-medium mb-1">
-              Velocidad: {playbackRate.toFixed(2)}x
-            </label>
-            <Slider
-              id="playback-rate"
-              min={0.25}
-              max={2}
-              step={0.25}
-              value={[playbackRate]}
-              onValueChange={handlePlaybackRateChange}
-            />
-          </div>
-          <Button onClick={replayVideo} className="flex items-center gap-2 w-full">
-            <Repeat className="w-4 h-4" />
-            Repetir
-          </Button>
+          {videoAvailable ? (
+            <>
+              <div className="aspect-square mb-2">
+                <ReactPlayer
+                  ref={playerRef}
+                  url={word.videoPalabra}
+                  width="100%"
+                  height="100%"
+                  playbackRate={playbackRate}
+                  playing={isPlaying}
+                  onReady={() => console.log('Video ready')}
+                  onError={(e) => console.error('Error loading video:', e)}
+                  onEnded={() => setIsPlaying(false)}
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="playback-rate" className="block text-sm font-medium mb-1">
+                  Velocidad: {playbackRate.toFixed(2)}x
+                </label>
+                <Slider
+                  id="playback-rate"
+                  min={0.25}
+                  max={2}
+                  step={0.25}
+                  value={[playbackRate]}
+                  onValueChange={handlePlaybackRateChange}
+                />
+              </div>
+              <Button onClick={replayVideo} className="flex items-center gap-2 w-full">
+                <Repeat className="w-4 h-4" />
+                Repetir
+              </Button>
+            </>
+          ):(
+            <AspectRatio className='my-4'>
+              <div className="flex items-center justify-center h-full">
+                <p className="text-lg font-medium text-primary-500">Video de referencia no disponible</p>
+              </div>
+            </AspectRatio>
+          )}
         </CardContent>
       </Card>
     </div>
